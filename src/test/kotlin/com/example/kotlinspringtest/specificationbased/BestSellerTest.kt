b57e69237a6dc8a2656afe7f037d1c7e1196286b
gosekose <ksyn1611@naver.com>
2023-09-06 01:15:04 +0900

    [Test] BestSeller 명세 기반 테스트 적용

--- src/test/kotlin/com/example/kotlinspringtest/specificationbased/BestSellerTest.kt
+++ src/test/kotlin/com/example/kotlinspringtest/specificationbased/BestSellerTest.kt
@@ -10,7 +10,39 @@ class BestSellerTest : BehaviorSpec({
         val bestSeller = BestSeller("GoseBook", 12)
 
 
-        `when`("비교할 책의 이름이 다르고, 수량이 베스트셀러 책보다 작을 때") {
+        `when`("비교할 책의 수량이 베스트셀러와 같고, 이름이 같을 때") {
+            val otherBookName = "GoseBook"
+            val otherSoldCount = 12
+            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
+
+            then("현재 베스트셀러가 유지되어야 해요") {
+                result shouldBe bestSeller
+            }
+        }
+
+        `when`("비교할 책의 수량이 베스트셀러와 같고, 이름이 빠를 때") {
+            val otherBookName = "CoseBook"
+            val otherSoldCount = 12
+            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
+
+            then("베스트셀러는 비교한 책으로 바뀌어야 해요") {
+                result shouldNotBe bestSeller
+                result.bookName shouldBe otherBookName
+                result.soldCount shouldBe otherSoldCount
+            }
+        }
+
+        `when`("비교할 책의 수량이 베스트셀러와 같고, 이름은 느릴 때") {
+            val otherBookName = "KoseBook"
+            val otherSoldCount = 12
+            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
+
+            then("현재 베스트셀러가 유지되어야 해요") {
+                result shouldBe bestSeller
+            }
+        }
+
+        `when`("비교할 책의 수량이 베스트셀러보다 적고, 이름은 다를 떄") {
             val otherBookName = "KoseBook"
             val otherSoldCount = 11
             val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
@@ -20,47 +52,15 @@ class BestSellerTest : BehaviorSpec({
             }
         }
 
-        `when`("비교할 책의 이름이 다르고, 수량이 베스트셀러 책보다 많을 때") {
+        `when`("비교할 책의 수량이 베스트셀러보다 많고, 이름은 다를 떄") {
             val otherBookName = "KoseBook"
             val otherSoldCount = 13
             val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
 
-            then("현재 베스트셀러가 비교할 책으로 바뀌어야 해요") {
+            then("베스트셀러는 비교한 책으로 바뀌어야 해요") {
                 result shouldNotBe bestSeller
                 result.bookName shouldBe otherBookName
                 result.soldCount shouldBe otherSoldCount
-            }
-        }
-
-        `when`("비교할 책의 이름은 사전순으로 느리고, 수량이 베스트셀러와 같을 떄") {
-            val otherBookName = "KoseBook"
-            val otherSoldCount = 12
-            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
-
-            then("현재 베스트셀러가 유지되어야 해요") {
-                result shouldBe bestSeller
-            }
-        }
-
-        `when`("비교할 책의 이름은 사전순으로 빠르고, 수량이 베스트셀러와 같을 떄") {
-            val otherBookName = "CoseBook"
-            val otherSoldCount = 12
-            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
-
-            then("현재 베스트셀러가 비교할 책으로 바뀌어야 해요") {
-                result shouldNotBe bestSeller
-                result.bookName shouldBe otherBookName
-                result.soldCount shouldBe otherSoldCount
-            }
-        }
-
-        `when`("비교할 책의 수량이 베스트셀러와 모두 같을 떄") {
-            val otherBookName = "KoseBook"
-            val otherSoldCount = 12
-            val result = bestSeller.getUpdateBestSeller(otherBookName, otherSoldCount)
-
-            then("현재 베스트셀러가 유지되어야 해요") {
-                result shouldBe bestSeller
             }
         }
     }
