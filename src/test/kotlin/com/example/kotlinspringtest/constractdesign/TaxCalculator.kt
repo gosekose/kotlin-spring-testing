2c2082370a9a3946b177d9a7c5eecb3dd5370ffc
gosekose <ksyn1611@naver.com>
2023-09-07 03:08:27 +0900

    [Test] 계약 설계 - 세금 계산

--- src/test/kotlin/com/example/kotlinspringtest/constractdesign/TaxCalculator.kt
+++ src/test/kotlin/com/example/kotlinspringtest/constractdesign/TaxCalculator.kt
@@ -1,20 +1,38 @@
 package com.example.kotlinspringtest.constractdesign
 
 class TaxCalculator {
-    fun calculateTax(amount: Double): Double {
+    fun calculateTax(amount: Double, countryTax: CountryTax): Double {
 
         require(amount >= 0) { throw TaxException(TaxErrorCode.INPUT_NOT_NEGATIVE)}
 
-        val taxAmount = amount * RATE
+        val taxAmount = amount * countryTax.rate
 
         require(taxAmount >= 0) { throw TaxException(TaxErrorCode.OUTPUT_NOT_NEGATIVE)}
 
         return taxAmount
     }
+}
 
-    companion object {
-        const val RATE = 1.1
-    }
+enum class Country(
+    val countryName: String,
+    val countryCode: String,
+) {
+    KOREA("Korea", "100"),
+    USA("United State America", "101");
+}
+
+enum class CountryTax(
+    val country: Country,
+    val rate: Double,
+) {
+    KOREA(Country.KOREA, 1.1),
+    USA(Country.USA, 1.2);
+
+    val countryCode: String
+        get() = country.countryCode
+
+    val countryName: String
+        get() = country.countryName
 }
 
 class TaxException(
--- src/test/kotlin/com/example/kotlinspringtest/constractdesign/TaxCalculatorTest.kt
+++ src/test/kotlin/com/example/kotlinspringtest/constractdesign/TaxCalculatorTest.kt
@@ -11,11 +11,12 @@ class TaxCalculatorTest : FunSpec({
     context("사전 조건: 입력이 음수가 입력되면") {
         test("TaxException이 발생하고 INPUT_NOT_NEGATIVE 메세지가 발생해요") {
             shouldThrow<TaxException> {
-                taxCalculator.calculateTax(-1.0)
+                taxCalculator.calculateTax(-1.0, CountryTax.KOREA)
             }.message.shouldBe(TaxErrorCode.INPUT_NOT_NEGATIVE.message)
         }
     }
 
+    /* 불변식의 경우 테스트를 하지 않고 비즈니스 로직을 해결하며 테스트 가능 */
     context("사후 조건: 결과가 음수가 나오면") {
         test("TaxException이 발생하고 OUTPUT_NOT_NEGATIVE 메세지가 발생해요") {
             val result = -1
