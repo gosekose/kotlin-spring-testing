64ea53089e18657d46f3e0d88f8d28acbf90ea74
gosekose <ksyn1611@naver.com>
2023-09-08 00:52:34 +0900

    [Test] 계약 설계 - 장바구니 수정 - product id로 동일성 검사

--- src/test/kotlin/com/example/kotlinspringtest/constractdesign/Basket.kt
+++ src/test/kotlin/com/example/kotlinspringtest/constractdesign/Basket.kt
@@ -90,7 +90,21 @@ data class Product(
     val name: String,
     val price: Long,
     val stock: Long,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Product
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 enum class BasketErrorCode(
     val code: String,
