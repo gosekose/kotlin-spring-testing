3a5131a070b6f2f078df9bc74176c5f259adfeee
gosekose <ksyn1611@naver.com>
2023-09-12 23:04:25 +0900

    [Feat] application Bean 등록 누락된 부분 추가

--- application/src/main/kotlin/com/order/application/port/out/OrderCommandPort.kt
+++ application/src/main/kotlin/com/order/application/port/out/OrderCommandPort.kt
@@ -1,7 +1,9 @@
 package com.order.application.port.out
 
 import com.order.model.Order
+import org.springframework.stereotype.Service
 
+@Service
 interface OrderCommandPort {
     fun saveOrder(order: Order): Long
 }
--- application/src/main/kotlin/com/order/application/service/OrderCreationService.kt
+++ application/src/main/kotlin/com/order/application/service/OrderCreationService.kt
@@ -3,7 +3,9 @@ package com.order.application.service
 import com.order.application.port.out.OrderCommandPort
 import com.order.usecase.OrderCreationRequest
 import com.order.usecase.OrderCreationUseCase
+import org.springframework.stereotype.Service
 
+@Service
 class OrderCreationService(
     private val orderCommandPort: OrderCommandPort,
     private val orderCreationUseCase: OrderCreationUseCase,
